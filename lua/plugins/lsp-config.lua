6eb188f69fadf0369e4de68ac017af2940ab8e21
Sunil Jain <suniljain0504@gmail.com>
2024-09-01 20:34:21 -0700

    added variable renaming hotkey

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -36,7 +36,9 @@ return {
             })
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
         end,
     },
 }
