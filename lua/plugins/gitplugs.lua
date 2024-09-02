de9f3faf0455882a05d0c6c8b23370c464da6a4a
Sunil Jain <suniljain0504@gmail.com>
2024-09-01 17:29:07 -0700

    forgot to update these files in last commit

--- lua/plugins/gitplugs.lua
+++ lua/plugins/gitplugs.lua
@@ -1,5 +1,10 @@
 return {
 	{
+        "tpope/vim-fugitive",
+        config = function()
+        end
+    },
+    {
 		"lewis6991/gitsigns.nvim",
 		config = function()
 			require("gitsigns").setup()
